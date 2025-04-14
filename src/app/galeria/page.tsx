0fa1fc381c4cca587a4c5101fa99e5d675e7b21b
Richard <richard@MacBook-Air-de-Richard.local>
2025-04-14 01:11:08 -0300

    Corrigindo erro de módulo na página galeria

---
+++ src/app/galeria/page.tsx
@@ -0,0 +1,10 @@
+// src/app/galeria/page.tsx
+
+export default function GaleriaPage() {
+    return (
+      <section className="min-h-screen bg-black text-[#dad1a0] flex items-center justify-center">
+        <h1 className="text-3xl text-[#dad1a0] uppercase">Página da Galeria</h1>
+      </section>
+    );
+  }
+  
